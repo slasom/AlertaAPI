08970a910e8e17d61258bcf301a0689d3c07d75a
Sergio Laso <slasoman@alumnos.unex.es>
2018-11-17 19:46:45 +0000

    Microservicio '/localizacion' implementado.

--- src/main/java/io/swagger/api/impl/LocalizacionApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/LocalizacionApiServiceImpl.java
@@ -2,7 +2,7 @@ package io.swagger.api.impl;
 
 import io.swagger.api.*;
 import io.swagger.model.*;
-
+import java.util.ArrayList;
 import io.swagger.model.Alerta;
 
 
@@ -22,12 +22,27 @@ import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen", date = "2018-11-17T17:57:23.078Z[GMT]")
 
 public class LocalizacionApiServiceImpl extends LocalizacionApiService {
+    ArrayList<Alerta> localizaciones = new ArrayList<>();
     
     @Override
     public Response localizacionPost(Alerta body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
-    }
+         //Check Objetc Alerta
+        if(body.getIdPersona() == null)
+            return Response.status(405).entity("Falta idPersona").build();
+        if(body.getFecha() == null)
+            return Response.status(405).entity("Falta fecha").build();
+        if(body.getHora() == null)
+            return Response.status(405).entity("Falta hora").build();
+        if(body.getLocalizacion() == null)
+            return Response.status(405).entity("Falta localizacion").build();
+        if(body.getLocalizacion().getLatitud() == null)
+            return Response.status(405).entity("Falta localizacion-latitud").build();
+        if(body.getLocalizacion().getLongitud() == null)
+            return Response.status(405).entity("Falta localizacion-longitud").build();
+        
+        localizaciones.add(body);
+        System.out.println("Add Localizacion");
+            
+        return Response.status(201).entity("La localizazion de la persona con ID:" + body.getIdPersona() + " ha sido guardada con exito!").build();}
     
 }
-
