f394cede5e693e7a6ef2ce9da86b70400bcef766
Sergio Laso <slasoman@alumnos.unex.es>
2018-11-18 17:48:46 +0000

    Los microservicios pertenecientes a las alertas (caídas, localización y batería) se han implementado.

--- src/main/java/io/swagger/api/impl/AlertaApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/AlertaApiServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
@@ -24,22 +25,69 @@ import javax.validation.constraints.*;
 
 public class AlertaApiServiceImpl extends AlertaApiService {
     
+    ArrayList<AlertaBateria> alertasBateria = new ArrayList<>();
+    ArrayList<Alerta> alertasCaidas = new ArrayList<>();
+    ArrayList<Alerta> alertasLocalizacion = new ArrayList<>();
+    
     @Override
     public Response alertaBateriaPost(AlertaBateria body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        if(body.getIdPersona() == null)
+            return Response.status(405).entity("Falta idPersona").build();
+        if(body.getFecha() == null)
+            return Response.status(405).entity("Falta fecha").build();
+        if(body.getHora() == null)
+            return Response.status(405).entity("Falta hora").build();
+        if(body.getNivelBateria() == null)
+            return Response.status(405).entity("Falta nivel batería").build();
+        
+        System.out.println("Add AlertaBateria");
+        alertasBateria.add(body);
+        
+        return Response.status(201).entity("La alerta de bateria perteneciente a ID:" + body.getIdPersona() + " ha sido guardada con exito!").build();
+    
     }
     
     @Override
     public Response alertaCaidaPost(Alerta body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        if(body.getIdPersona() == null)
+            return Response.status(405).entity("Falta idPersona").build();
+        if(body.getFecha() == null)
+            return Response.status(405).entity("Falta fecha").build();
+        if(body.getHora() == null)
+            return Response.status(405).entity("Falta hora").build();
+        if(body.getLocalizacion() == null)
+            return Response.status(405).entity("Falta localizacion").build();
+        if(body.getLocalizacion().getLatitud() == null)
+            return Response.status(405).entity("Falta localizacion-latitud").build();
+        if(body.getLocalizacion().getLongitud() == null)
+            return Response.status(405).entity("Falta localizacion-longitud").build();
+            
+        System.out.println("Add AlertaCaida");
+        alertasCaidas.add(body);
+        
+        return Response.status(201).entity("La alerta de caida perteneciente a ID:" + body.getIdPersona() + " ha sido guardada con exito!").build();
+    
     }
     
     @Override
     public Response alertaLocalizacionPost(Alerta body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        if(body.getIdPersona() == null)
+            return Response.status(405).entity("Falta idPersona").build();
+        if(body.getFecha() == null)
+            return Response.status(405).entity("Falta fecha").build();
+        if(body.getHora() == null)
+            return Response.status(405).entity("Falta hora").build();
+        if(body.getLocalizacion() == null)
+            return Response.status(405).entity("Falta localizacion").build();
+        if(body.getLocalizacion().getLatitud() == null)
+            return Response.status(405).entity("Falta localizacion-latitud").build();
+        if(body.getLocalizacion().getLongitud() == null)
+            return Response.status(405).entity("Falta localizacion-longitud").build();
+            
+        System.out.println("Add AlertaLocalizacion");
+        alertasLocalizacion.add(body);
+            
+        return Response.status(201).entity("La alerta de localizazion perteneciente a ID:" + body.getIdPersona() + " ha sido guardada con exito!").build();
     }
     
 }
